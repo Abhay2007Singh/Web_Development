b4e69541d24a74b19d08b5e4b5ded22ce1e4fc82
Abhay2007Singh <72340300+Abhay2007Singh@users.noreply.github.com>
2024-10-09 17:56:15 +0530

    Add files via upload

---
+++ Project_4/index.js
@@ -0,0 +1,11 @@
+let random=parseInt((Math.random()*100)+1);
+
+const submit=document.querySelector("submit");
+const form=document.querySelector("form");
+const Previous=document.querySelector("Previous");
+const remaining=document.querySelector("remaining");
+
+submit.addEventListener('submit',function(e){
+    e.preventDefault()
+    let container = parseInt(document.querySelector("#container").value);
+});
